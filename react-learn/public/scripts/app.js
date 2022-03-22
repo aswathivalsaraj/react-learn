f3feeb7721d0901098b492fdb01d1dd50ebae590
aswathivalsaraj <mail.aswathivalsaraj@gmail.com>
2022-03-22 14:10:22 +0530

    Ignore file added

--- react-learn/public/scripts/app.js
+++ react-learn/public/scripts/app.js
@@ -1,1 +1,20 @@
-console.log("React app is running")
+"use strict";
+
+console.log("App is running");
+var template = React.createElement(
+  "header",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    "This is JSX from src/app.js "
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Again a paragraph"
+  ),
+  " "
+);
+var appRoot = document.getElementById("test");
+ReactDOM.render(template, appRoot);
